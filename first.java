4c226b10f3640249c6e4912ca622c18ab1f80a30
igimot <oleg.chulanov@gmail.com>
2016-05-18 11:40:20 +0300

    Домашнее задание к Модулю 1

---
+++ first.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Oleg on 18.05.2016.
+ */
+public class first {
+    public static void main(String[] arg){
+        System.out.println("Hi ther its my first coding in java. Coool");
+        System.out.println("and");
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
